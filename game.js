108ffa19d60301c1cfe3e8230a271f048a08b15a
darkfireonline <37456899+darkfireonline@users.noreply.github.com>
2018-03-16 16:51:36 -0500

    3

--- game.js
+++ game.js
@@ -1,11 +1,14 @@
-var fso = new ActiveXObject("Scripting.FileSystemObject");
-alert("Enter your username");
-var userName = prompt();
-if(fso.FileExists("C:\\Users\\Overlord Squidian\\Desktop\\index\\saves"))
-{
-	alert("It worked");
-}
-else
-{
-  alert("It did not work")
-}
+var choice = prompt("This is a test");
+alert(choice);
+console.log("test2");
+//var fso = new ActiveXObject("Scripting.FileSystemObject");
+//alert("Enter your username");
+//var userName = prompt();
+//if(fso.FileExists("C:\\Users\\Overlord Squidian\\Desktop\\index\\saves"))
+//{
+//	alert("It worked");
+//}
+//else
+//{
+//  alert("It did not work")
+//}
