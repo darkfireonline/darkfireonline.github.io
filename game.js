5bb1812f4178641e4bcb230eb27610f3d98d66db
darkfireonline <37456899+darkfireonline@users.noreply.github.com>
2018-03-16 16:39:25 -0500

    2.5
    Fixed undo

---
+++ game.js
@@ -0,0 +1,11 @@
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+alert("Enter your username");
+var userName = prompt();
+if(fso.FileExists("C:\\Users\\Overlord Squidian\\Desktop\\index\\saves"))
+{
+	alert("It worked");
+}
+else
+{
+  alert("It did not work")
+}
