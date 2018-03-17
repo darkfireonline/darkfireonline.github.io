6629170d8b42b57d46a402706d614ac4955f5098
darkfireonline <37456899+darkfireonline@users.noreply.github.com>
2018-03-16 19:52:50 -0500

    7

--- game.js
+++ game.js
@@ -1,11 +1,22 @@
 var fso = new ActiveXObject("Scripting.FileSystemObject");
 //alert("Enter your username");
 //var userName = prompt();
-if(fso.FileExists("https://www.google.com/"))
+function doesFileExist(urlToFile) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('HEAD', urlToFile, false);
+    xhr.send();
+
+    if (xhr.status == "404") {
+        return false;
+    } else {
+        return true;
+    }
+}
+if(fso.FileExists("https:\\www.google.com\\"))
 {
-	alert("It worked");
+	alert("It worked2");
 }
 else
 {
-  alert("It did not work")
+  alert("It did not work2")
 }
